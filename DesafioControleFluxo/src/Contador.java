ee1bffecb642191752aea9b1014026e253eda92a
andersonbarretoalves <andersonguitar2@gmail.com>
2023-04-08 15:04:34 -0300

    final

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,26 +1,40 @@
 import java.util.Scanner;
-
 public class Contador {
 	public static void main(String[] args) {
+
 		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
-		
-		try {
+			System.out.println("Digite o primeiro parâmetro");
+			int parametroUm = terminal.nextInt();
+			System.out.println("Digite o segundo parâmetro");
+			int parametroDois = terminal.nextInt();
+			terminal.close();
+	
+			try {
 			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);
-		
-		}catch (? exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			contar(parametroUm, parametroDois);			
+			
+			}catch (ParametrosInvalidosException e) {
+				System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+				//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			}
 		}
 		
-	}
+		
+
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		
-		int contagem = parametroDois - parametroUm;
+		if (parametroUm < parametroDois) {
+
+
+		//int contagem = parametroDois - parametroUm;
+		for (int contagem = parametroDois - parametroUm; contagem > 0; contagem --)
+
 		//realizar o for para imprimir os números com base na variável contagem
+		System.out.println("Parametro um: " + parametroUm + ", Parametro dois: " + parametroDois);
+		}	
+		else {
+		throw new ParametrosInvalidosException();
+		}
+			
 	}
-}
+} 
